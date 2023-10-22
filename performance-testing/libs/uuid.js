566c3d186caaff6bb643c9fbcf3055de3236957a
rcalves82 <rclarindo.alves@gmail.com>
2023-10-22 02:26:35 -0300

    finalizando o readme

--- performance-testing/libs/uuid.js
+++ performance-testing/libs/uuid.js
@@ -9,10 +9,12 @@ uuid.v4 = v4;
 module.exports = uuid;
 
 },{"./v1":4,"./v4":5}],2:[function(require,module,exports){
+  
 /**
  * Convert array of 16 byte values to UUID string format of the form:
  * XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX
  */
+
 var byteToHex = [];
 for (var i = 0; i < 256; ++i) {
   byteToHex[i] = (i + 0x100).toString(16).substr(1);
